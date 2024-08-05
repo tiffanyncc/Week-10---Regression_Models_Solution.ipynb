6f70a895bc5f3edcd42ae74f019b11fcdd9c2c94
tiffanyncc <tiffany.ncc.2@gmail.com>
2024-08-04 20:44:00 -0400

    updated

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -6,7 +6,7 @@ def plot_tree(model, feature_names):
     try:
         plt.figure(figsize=(20, 10))
         tree.plot_tree(model, feature_names=list(feature_names))
-        plt.savefig('tree.png', dpi=300)  # Save the plot to a file first
+        plt.savefig('src/visualization/images/tree.png', dpi=300)  # Save the plot to a file first
         logging.info('Decision Tree plot saved successfully.')
         plt.show()  # This will pop up the window with the tree plot
         logging.info('Decision Tree plot displayed successfully.')
